1b9e23bc4e9cf666d88d8b3df564fc1d9c99474b
rainnus <neverlament@gmail.com>
2014-04-26 13:38:19 +0800

    Revert "first commit"
    This reverts commit 57e794ae356815187794517b0caafc9c626c5604.

--- ticket_backend/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class ActivityForm(forms.Form):
-    caption = forms.CharField(label='caption', max_length=100)
-    destruction = forms.CharField(widget=forms.Textarea)
-    
-
-class CatagoryForm(forms.Form):
-    catagory_name = forms.CharField()
